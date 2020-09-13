3b0ad80596396b0d9516d44f06127be86eecf182
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-13 17:13:34 +0100

    changed display and size properties of ProjectTile components on home page

--- changelog.md
+++ changelog.md
@@ -1,9 +1,10 @@
 (Major, Minor, Patch Versioning)
 
-# LOG 0.1.0 (13/09/2020)
-- Updated Readme - 16:10
-- Added changelog - 16:10
-- Created develop branch - 16:10
-- Created pre-dev branch - 16:36
-- home.js 'About Me' button wrapped in class and styled - 16:47
-- added css box-sizing to project-tile components - 17:03
+# LOG 0.1.0 (13/09/2020 - ??/??/??)
+- Updated Readme - (16:10 - 13/09/2020)
+- Added changelog - (16:10 - 13/09/2020)
+- Created develop branch - (16:10 - 13/09/2020)
+- Created pre-dev branch - (16:36 - 13/09/2020)
+- home.js 'About Me' button wrapped in class and styled - (16:47 - 13/09/2020)
+- added css box-sizing to project-tile components - (17:03 - 13/09/2020)
+- changed display and size properties of ProjectTile components on home page - (17:12 - 13/09/2020)
--- src/pages/home.js
+++ src/pages/home.js
@@ -8,18 +8,18 @@ function HomePage(props) {
     return (
         <div className="container mt-5 d-flex flex-column h-100">
             <div className="row justify-content-between">
-                <ProjectTile title="IMS Project" colSize="col-md-4 col-lg-3" />
-                <div className="col-md-4 col-lg-6 mb-3">
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <div className="col-md-4 col-lg-6 mb-3 d-none d-lg-block">
                     <button id="HomeAboutBtn" className="btn btn-light btn-block h-100"><h1 className="display-2">About Me</h1></button>
                 </div>
                 {/* <HeaderBox title="About Me" colSize="col-md-4 col-lg-6" /> */}
-                <ProjectTile title="IMS Project" colSize="col-md-4 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
             </div>
             <div className="row justify-content-between">
-                <ProjectTile title="IMS Project" colSize="col-12 col-md-5 col-lg-3" />
-                <ProjectTile title="IMS Project" colSize="col-12 col-md-5 col-lg-3" />
-                <ProjectTile title="IMS Project" colSize="col-12 col-md-5 col-lg-3" />
-                <ProjectTile title="IMS Project" colSize="col-12 col-md-5 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
             </div>
 
         </div>

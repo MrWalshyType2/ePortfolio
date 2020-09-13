7833fcf88cf07f27e601aab93aa9f479c7ba995e
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-13 17:30:50 +0100

    removed HeaderBox import

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ProjectTile from '../components/project-tile';
-import HeaderBox from '../components/header-box';
 import { Link } from 'react-router-dom';
 
 function HomePage(props) {

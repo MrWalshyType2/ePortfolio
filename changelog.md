f210bdf8f5a141d0eede25b52a99b53071df8f1c
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-13 17:28:23 +0100

    wrapped about me button with Link component

--- changelog.md
+++ changelog.md
@@ -5,6 +5,4 @@
 - Added changelog - (16:10 - 13/09/2020)
 - Created develop branch - (16:10 - 13/09/2020)
 - Created pre-dev branch - (16:36 - 13/09/2020)
-- home.js 'About Me' button wrapped in class and styled - (16:47 - 13/09/2020)
-- added css box-sizing to project-tile components - (17:03 - 13/09/2020)
-- changed display and size properties of ProjectTile components on home page - (17:12 - 13/09/2020)
+- home page created and templated (17:19 - 13/09/2020)
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ProjectTile from '../components/project-tile';
 import HeaderBox from '../components/header-box';
+import { Link } from 'react-router-dom';
 
 function HomePage(props) {
 
@@ -10,7 +11,7 @@ function HomePage(props) {
             <div className="row justify-content-between">
                 <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
                 <div className="col-md-4 col-lg-6 mb-3 d-none d-lg-block">
-                    <button id="HomeAboutBtn" className="btn btn-light btn-block h-100"><h1 className="display-2">About Me</h1></button>
+                <Link to="/about"><button id="HomeAboutBtn" className="btn btn-light btn-block h-100"><h1 className="display-2">About Me</h1></button></Link>
                 </div>
                 {/* <HeaderBox title="About Me" colSize="col-md-4 col-lg-6" /> */}
                 <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />

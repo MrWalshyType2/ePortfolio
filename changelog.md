893f8c8aac2940a223ec7ed0b1e050465e7f7e38
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-16 16:34:36 +0100

    Created project page and templated

--- changelog.md
+++ changelog.md
@@ -6,3 +6,4 @@
 - Created develop branch - (16:10 - 13/09/2020)
 - Created pre-dev branch - (16:36 - 13/09/2020)
 - home page created and templated (17:19 - 13/09/2020)
+- project page created and templated
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ProjectTile from '../components/project-tile';
 
 function ProjectPage(props) {
 
     return (
-        <h1>Project</h1>
+        <div className="container mt-5 d-flex flex-column h-100">
+            <h1>Projects</h1>
+            <div className="row justify-content-between mt-5">
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="PEP Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="ePortfolio" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+            </div>
+            <div className="row justify-content-between mt-5">
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+                <ProjectTile title="IMS Project" colSize="col-12 col-md-6 col-lg-3" />
+            </div>
+        </div>
     );
 }
 

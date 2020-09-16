c18e4bf16589768f060be2236326d383d9bc2c62
MrWalshyType2 <67016030+MrWalshyType2@users.noreply.github.com>
2020-09-16 16:37:12 +0100

    Revert "Feature about page"

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,19 +3,7 @@ import React from 'react';
 function AboutPage(props) {
 
     return (
-        <div className="container mt-5 d-flex flex-column h-100">
-            <div className="row justify-content-between">
-                <img className="col-md-3 d-none d-md-block rounded-circle" src="https://via.placeholder.com/200" alt="..." />
-                <div className="col-12 col-md-8 bg-light rounded-lg material-shadow">
-                    <p>ofijaowiejfowjf0j4390jf 4jr094j3f093 jjf4f0j20jf09j2fjef9 </p>
-                </div>
-            </div>
-            <div className="row my-5">
-                <div className="col-12 bg-light rounded-lg material-shadow" style={{"height": "300px"}}>
-                    <p>0jfr0ja9fhvj j890urw98uf8w9u </p>
-                </div>
-            </div>
-        </div>
+        <h1>About</h1>
     );
 }
 
